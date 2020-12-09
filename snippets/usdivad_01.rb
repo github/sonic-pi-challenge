e31fdf77749659a853870d3113e3b949f2e4333c
David Su <david.d.su@gmail.com>
2020-12-08 20:14:05 -0800

    adjust some gains in sonnet snippet

--- snippets/usdivad_01.rb
+++ snippets/usdivad_01.rb
@@ -1,13 +1,13 @@
 # Sonic Pi Sonnet 01 by David Su (@usdivad)
 
 use_synth :dark_ambience
-play (scale :E, :minor_pentatonic), amp: 1, attack: 1, release: 7
+play (scale :E, :minor_pentatonic), amp: 3, attack: 1, release: 7
 
 use_synth :beep
 32.times do
-  with_fx :panslicer, wave: 1 do
+  with_fx :panslicer, wave: 1, pre_amp: 0.8 do
     with_fx :vowel, vowel_sound: [1,2,3,4,5].choose, voice: 3 do
-      play (scale :E, :minor_pentatonic).choose, amp: rrand(0.1, 0.5), release: [0.25, 0.5].choose
+      play (scale :E, :minor_pentatonic).choose, amp: rrand(0.1, 0.9), release: [0.25, 0.5].choose
       sleep [0.125, 0.25].choose
     end
   end
